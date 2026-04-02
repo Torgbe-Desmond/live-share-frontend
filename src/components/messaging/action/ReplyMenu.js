ab1318483580b641eca764bf446b2ab23c8f8600
Torgbe-Desmond <123torgbe@gmail.com>
2026-04-02 13:15:50 +0000

    fixed bug

--- src/components/messaging/action/ReplyMenu.js
+++ src/components/messaging/action/ReplyMenu.js
@@ -4,7 +4,6 @@ import ReplyIcon from "@mui/icons-material/Reply";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { useTheme } from "@mui/material/styles";
 import { Box, Tooltip, useMediaQuery, Paper } from "@mui/material";
-import DownloadIcon from "@mui/icons-material/Download";
 
 export default function ReplyMenu({
   msg,
