03e0ac71544d069de7a42d44afbcc0b197f3f6ef
123torgbe@gmail.com <123torgbe@gmail.com>
2026-04-24 11:34:58 +0000

    changed theme and backend api endpoint

--- src/api/URI.js
+++ src/api/URI.js
@@ -1,6 +1,6 @@
 export const API_BASE =
   process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test"
-    ? "https://live-share-wpw8.onrender.com"
+    ? "https://live-share-2jnx.onrender.com"
     : "http://localhost:5000";
 
 export const API_URL =
